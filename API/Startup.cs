4635e936ebcf2b16342972fbdd9b2f6e548d417d
osaelh <oussamaelh@gmail.com>
2021-06-24 15:46:42 +0100

    CorsPolicy

--- API/Startup.cs
+++ API/Startup.cs
@@ -40,6 +40,14 @@ namespace API
             {
                 opt.UseSqlite(_configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin();
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +63,8 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
