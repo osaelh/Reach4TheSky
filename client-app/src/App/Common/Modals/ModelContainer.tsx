ce965faadb476d32c5081c7ceb83869243332343
osaelh <oussamaelh@gmail.com>
2021-10-26 19:40:27 +0100

    Client side regitration validation

--- client-app/src/App/Common/Modals/ModelContainer.tsx
+++ client-app/src/App/Common/Modals/ModelContainer.tsx
@@ -8,7 +8,7 @@ export default observer(function(){
     const {modalStore} = useStore();
 
     return (
-        <Modal open={modalStore.modal.open} onClose={modalStore.closeModal} size='mini'>
+        <Modal open={modalStore.modal.open} onClose={()=>modalStore.closeModal()} size='mini'>
             <Modal.Content>
                 {modalStore.modal.body}
             </Modal.Content>
--- client-app/src/Features/Users/RegisterForm.tsx
+++ client-app/src/Features/Users/RegisterForm.tsx
@@ -4,6 +4,7 @@ import { Button, Header, Label } from 'semantic-ui-react';
 import MyTextInput from '../../App/Common/Form/MyTextInput';
 import { useStore } from '../../App/Stores/store';
 import * as Yup from 'yup';
+import ValidationErrors from '../errors/ValidationErrors';
 
 
 export default observer( function RegisterForm(){
@@ -12,7 +13,7 @@ export default observer( function RegisterForm(){
     return (
         <Formik
         initialValues={{displayName: '', email:'' ,username : '', password: '',error:null}}
-        onSubmit={(values, {setErrors}) => userStore.register(values).catch(err => setErrors({error: "Invalid email or username" }))}
+        onSubmit={(values, {setErrors}) => userStore.register(values).catch(error => setErrors({error }))}
         validationSchema={Yup.object({
             displayName: Yup.string().required(),
             username: Yup.string().required(),
@@ -21,14 +22,14 @@ export default observer( function RegisterForm(){
         })}
         >
             {({handleSubmit, isSubmitting, errors, isValid , dirty}) => (
-                <Form className="ui form" onSubmit={handleSubmit} autoComplete='off'>
+                <Form className="ui form error" onSubmit={handleSubmit} autoComplete='off'>
                     <Header as='h2' content='Sign up to events!' color='teal'/>
                     <MyTextInput name='email' placeholder='Email' />
                     <MyTextInput name='displayName' placeholder='Dispalyname' />
                     <MyTextInput name='username' placeholder='Username' />
                     <MyTextInput name='password' placeholder='Password' type='password' />
                     <ErrorMessage name='error'
-                      render={()=> <Label style={{marginBottom: '10px'}} color='red' basic content={errors.error} />}
+                      render={()=> <ValidationErrors  errors={errors.error}/>}
                       />
                     <Button disabled={!isValid || !dirty || isSubmitting} loading={isSubmitting} positive content="Register" type='submit' fluid />
                 </Form>
