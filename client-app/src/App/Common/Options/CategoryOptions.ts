bed43f4fee8ec6fac304d5d9181f04562c3cc5ec
osaelh <oussamaelh@gmail.com>
2021-12-23 23:27:53 +0100

    Adding images

--- client-app/src/App/Common/Options/CategoryOptions.ts
+++ client-app/src/App/Common/Options/CategoryOptions.ts
@@ -1,6 +1,6 @@
 export const categoryOption = [
     {text: 'Meteor', value:'meteor'},
-    {text: 'Solar and lunar eclipses', value:'solar and lunar eclipses'},
-    {text: 'Comet flybys', value:'comet flybys'},
+    {text: 'Solar and lunar eclipses', value:'solar_and_lunar_eclipses'},
+    {text: 'Comet flybys', value:'comet_flybys'},
     {text: 'Solstices', value:'solstices'}
 ];
